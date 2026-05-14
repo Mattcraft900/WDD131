d0fd1bb7be4ef8cd77f0388e92f584801168a980
Mattcraft900 <mattcraft900@gmail.com>
2026-05-14 11:16:14 -0600

    Did the responsive-gallery ponder assn

--- ponder/responsive-gallery/script.js
+++ ponder/responsive-gallery/script.js
@@ -7,10 +7,21 @@ const closeButton = modal.querySelector('.close-viewer');
 // Event listener for opening the modal
 gallery.addEventListener('click', openModal);
 
-function openModal(e) {
-    
 // Code to show modal  - Use event parameter 'e'   
-    
+function openModal(e) {
+    // .target accesses the DOM element that was clicked on (specifics pending, asked AI)
+    // .src gives the 'src' attribute of that elemtn - which works for img's, a's, et.c because they have a 'src' attribute
+    //      (the src returned is the full filepath)
+    console.log(e.target.src);
+    let imageSrc = e.target.src;
+
+    // Manipulate the file name to get the bigger version of the clicked-on image
+    let bigImageSrc = imageSrc.replace("-sm", "-full")
+
+    // Remember that properties like '.src' works for both reading and writing
+    modalImage.src = bigImageSrc;
+    // Remember that the opposite of .showModal() is .close() [yeah it's silly]
+    modal.showModal();
 }
 // Close modal on button click
 closeButton.addEventListener('click', () => {
