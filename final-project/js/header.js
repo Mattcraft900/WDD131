f824d2f51a5e52cc05d609b2f43622b3811f9720
Mattcraft900 <mattcraft900@gmail.com>
2026-07-06 14:13:33 -0600

    Fixed header generation

--- final-project/js/header.js
+++ final-project/js/header.js
@@ -1,25 +1,32 @@
 const header = document.querySelector('header');
-const menuBtn = document.getElementById('menu-btn');
-const nav = document.querySelector('nav');
-const menuList = document.getElementById('menu-list');
-
 
 // Insert header HTML upon page load (happens on every page)
 document.addEventListener('DOMContentLoaded', function insertHeader() {
-    header.innerHTML = `
-        <button id="menu-btn">=</button>
-        <nav class="hidden">
-            <ul id="menu-list">
-                <li class="nav-link"><a href="index.html">Home</a></li>
-                <li class="nav-link"><a href="bio-selection.html">Characters</a></li>
-                <li class="nav-link"><a href="travelogue.html">Lucy's Travelogue</a></li>
-            </ul>
-        </nav>
-    `
-});
+    // Build the header's inner DOM elements
+    const menuBtn = document.createElement('button');
+    menuBtn.id = 'menu-btn';
+    menuBtn.innerHTML = `
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+    `;
+    const navMenu = document.createElement('nav');
+    const menuList = document.createElement('ul');
+    menuList.id = 'menu-list';
+    menuList.innerHTML = `
+        <li class="nav-link"><a href="index.html">Home</a></li>
+        <li class="nav-link"><a href="bio-selection.html">Characters</a></li>
+        <li class="nav-link"><a href="travelogue.html">Lucy's Travelogue</a></li>
+    `;
 
-// When menu button is clicked, toggle sidebar menu options
-menuBtn.addEventListener('click', e => {
-    menuBtn.attributes.toggle('opened');
-    nav.classList.toggle('hidden');
-})
+    // Add the event listener to the menu button
+    menuBtn.addEventListener('click', e => {
+        menuBtn.attributes.toggle('opened');
+        navMenu.classList.toggle('hidden');
+    });
+    
+    // Attach the elements to the DOM
+    header.appendChild(menuBtn);
+    header.appendChild(navMenu);
+    navMenu.appendChild(menuList);
+});
