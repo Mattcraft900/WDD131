7f16b876e83f6a33f0842442d047743c9c87a85a
Mattcraft900 <mattcraft900@gmail.com>
2026-07-15 19:49:19 -0600

    Added travolgue entries

---
+++ final-project/js/travelogue-entries.js
@@ -0,0 +1,71 @@
+export const entries = [
+    {
+        "session-date": "Prologue",
+        "content": [
+            {
+                "game-date": null,
+                "text": [
+                    "Hi, my name is Lucy and this is my journal. Kind of. Maybe. My friend Nemah is teaching me how to write, so I thought it'd be good practice if I started keeping a record of everything that happens to us. Like, to the Undying Lights, I mean. (I think that's what our name should be, by the way, and if I'm the one writing it, I get to choose. I think.) <You… sure wrote it.> So I guess this is kinda like a diary, but like for all of us? Except I think people put like, real deep secret stuff in their diary, and I ain't doing that! That's what my little tome is for. <Tome?>",
+                    "Anyhow, I don't think anyone else is keeping a record of what's happening, but I kinda think someone should. I know Enza at least would appreciate that. I feel bad for him, what with… everything that's happened and all. I thought maybe someday our story could be told all over, just like that Gentleman George guy I've heard him talk about. <Do not idolize those racist stories.> So why not start writing it myself?",
+                    "Well, actually… I bet that they probably want the words to be all nice and pretty-like for that kinda thing, and I'm still real bad at spelling and all that… <There aren't that many… …anymore.> but I could always ask Nemah to fix all the mistakes for me. <Will do.> And come to think of it, it might be a good idea to ask her to add stuff that she remembers, too, because there ain't no way I'm gonna catch everything. In fact maybe I should ask Luark, too?? He notices everything, I swear. I'll just ask Nemah at first and see how that goes, I guess.",
+                    "I came back to this after a few hours and I realized: if people are gonna be reading this they're gonna be missin' a lotta information. Heck, I'M missin' a lotta information… ugh. Well, I ain't about to ask the gang about all the little details of the story up to this point. Zol only knows I already gotta be careful about our conversation topics, and generally speakin', the past is NOT a great one to try out most of the time. Maybe I can just, like, make a little list of who we are and stuff like that, and probably some of the other people that are important, too… I guess I'll have to ask Nemah to go over that stuff too, then. I hope she doesn't mind. <Not at all.>",
+                ],
+            },
+        ],
+    },
+    {
+        "session-date": "6.13.26 - \"Saint Dane\"",
+        "content": [
+            {
+                "game-date": "7 / 13 / 568",
+                "text": [
+                    "Today Nemah taught me how to write. It takes me a long time to write anything right now because I am still learning. Once I get a little better I want to write a proper introduction, but this is enough for now.",
+                    "Right now, we're on a fairy ride down to Saint Dane (apparently a fairy is also a kind of boat?), <Ferry, they're spelled different.> but it will take us a few days to get there. This is the calmest time we've had since… since these guys found me, I think.",
+                    "Today was as crazy as usual. We spent last night in the Hollow, but I didn't get much sleep because Enza turned me into a plant. (I strangely don't feel sleepy, though. I must just be excited to be traveling somewhere new again.) We left through the fountain in Mercy's Bones since it's a little closer to Saint Dane and Shen Ora.",
+                    "Enza and Chesco went to visit one of Chesco's college friends, but me and Luark and Nemah all went shopping for supplies for the trip. I could be wrong but it kinda felt like Nemah & Luark aren't as experienced at wilderness survival as I would've expected. <I'm great at it actually. You just need supplies… of some sort.> I know I'm more experienced than most with securing ropes and such, but still. It's a good thing they're so physically capable. Regardless, while we were there I finally gave in and bought some leather armor, and I also bought a cute hat to help keep the sun off! Luark treated us to some food and I learned what a baked potato is (they don't have any bread on them at all). They're very tasty.",
+                    "We decided we should go check on the other two, finally, and it's a good thing we did, because when we reached the university there were a few officers talking to Chesco, and a whole bunch of 'em staring up at Enza, who was standing on a second-story window ledge and totally drunk! <He was not actually drunk.> Nemah came to the rescue in a magic disguise and got Enza outta there with just a fine, thankfully. <I should have left him there, make him clean up his own mess.> We grilled the two of 'em about what happened, and for some reason Chesco was very intent on convincing us that his old roommate is dead -- even though that's already what the officers had already told us. He might be the worst liar I've ever heard. <Evidently, they're somewhere in Shen Ora. I have no idea why he was lying about it.>",
+                    "With that out of the way, we found this fairy going down the river, and that's where we'll be for a bit. Nemah has been complaining about the humidity of the swamplands, but I think it's nice. <No, it's not.> It reminds me of the few times it would rain out in the Mesas…",
+                ],
+            },
+            {
+                "game-date": "7 / 15 / 568",
+                "text": [
+                    "We get off the boat today, in a few hours. Everyone's been… quiet. Luark is more gloomy than usual; his wife might be in Shen Ora, and I can tell he's worried about what it'd be like to finally meet her again after all this time. On the other hand, Enza is basically petrified by the thought of seeing his parents again, but I just know it'll be so good for him. He needs this. I'm mostly worried about Chesco; he's pointed out a few times that most of the goblins in Saint Dane (which is a lot of goblins, I think) would want to hurt him, and it's rare for him to stay focused on a topic like that - especially without his usual big grin. Nemah has been teaching me more readin' and writin' skills. <Hopefully we just pass through unnoticed.>",
+                    "My brain hurts, but I didn't write anything yesterday (I just doodled instead), and Nemah scolded me for it and said I have no reason to skip out on practicing today. Personally, I can think of plenty of reasons. <If you want to learn, you need practice.> But… the way she said it… I guess it reminds me of Luke.",
+                ],
+            },
+            {
+                "game-date": "7 / 17 / 568 [or later]",
+                "text": [
+                    "Things did NOT go well in Saint Dane. They went very, very bad, actually. <Very bad.>",
+                    "When we arrived, Enza was lookin' downright like a ghost, the poor thing. He recognized the terrain enough that he led us toward the city proper, then to the front of a little restaurant. There were ambystomans and goblins everywhere. I held Chesco's hand because I was afraid he might take off running if I didn't, the way he was shaking and all. Poor thing.",
+                    "Enza started talkin' to a little boy out in front of the restaurant who looked just like him, but smaller. The boy said he had an older sister, too. (When Enza left home, he was an only child.) Enza did so well for himself, though, keeping it together. Of course he woulda been fine crying, but you could just tell it was important to him. Anyway, his brother didn't recognize his face or react to his name, which was a little worrisome, but he moved inside to wait in line where his mom was taking orders. We all got in line behind him for support (except for Luark, who had gone in ahead and ordered a buncha food for us).",
+                    "Enza finally got to the front, and… his mom didn't recognize him, neither. Then out of nowhere, this smug goblin, Luka, shows up, says something smarmy <?> or whatever, and vanishes with Enza!",
+                    "That's when things start to get a little blurry for me… I remember running outside, all of us, and next thing I know, Enza comes flying' down the street and crashes into a brick wall. I'll never forget seeing his eyes as the dust cleared… he was giving up. And he was furious.",
+                    "<Enza looked as though he wanted a bloodbath. So much for passing by unnoticed…>",
+                ],
+            },
+        ],
+    },
+    {
+        "session-date": "6.27.26 - \"The Orphanage\"",
+        "content": [
+            {
+                "game-date": null,
+                "text": [
+                    "All of us split up at that point. Enza started walking determinedly toward… wherever he came from, and Nemah started after him. I was holding Chesco's hand, and he started slowly walking forward, so I stuck with him. Luark stayed behind, though. I never did ask him what he was doing. I don't know if I should.",
+                    "Anyway, by the time Chesco and I got to the manor, we had lost sight of Nemah, but there was a clear path of carnage through the front gates and in through the doors. Dead goblins, mostly. Chesco was shaking like a trapped cicada. I wasn't feeling very good myself.",
+                    "We looked around a little. Like I said, everything is… fuzzy for me. A kid maybe four years old came out of a room in the big hallway. The sun was already setting by this point, and he looked sleepy. I told him everything was fine. There were bodies all around, but he couldn't see them. I'm still not really clear on the details, but it turns out we were in an orphanage of some kind? I've always heard of them, but I didn't know what they looked like.",
+                    "Then… there was an explosion. Chesco and I were both kinda shocked out of our stupors, I think. We still hadn't seen Enza or Nemah since we came in. Chesco suddenly jumped into action, and of course I had to follow him! It quickly became apparent that there was a huge fire.",
+                    "I'm writing this a day or two after it all went down, but at the time, I really thought Enza had started it. It doesn't really make sense in hindsight; his whole thing is water and mist and stuff, obviously. But I really thought…",
+                    "Either way, Chesco was incredible. He just leaped through the flames without any hesitation, off to save some kids. I don't even know how he knew they were there! It's tricky to tell exactly what happened -- the story sounds a different kind of crazy every time he tells it -- but it sounds like he shot a man who worked at the orphanage for some reason, then helped all the kids out of a second-story window, and then scampered off to the kitchen to save another kid, who Chesco insists is really good at eating peanut butter. Then once everyone was safe, he entertained the kids so they wouldn't be scared! He was like… a Lucy, when the real Lucy stopped working. I'm so proud of him!",
+                    "But before that… Nemah died.",
+                    "I guess she and Enza had left the building and came back once it was on fire, and she -- just like Chesco -- was a total hero and dove right in to save people. But… I don't know what happened. Maybe she can say, herself. <No.>",
+                    "Whatever the case, we all tried to save her. But we were seconds too late. (I know I've controlled flames before, but I just couldn't get it to work! Stupid good-for-nothing sonuvagun devil) So… Yeah. She's alive now, because, you know. Deathless. But that happened.",
+                    "Anyway, Chesco was playing with the kids and Enza and I were both sulking on our own when Luark showed up. He gave Enza his hat back (which Nemah took with her when she died?), and talked to him a bit, then Chesco. I don't really know what he said but then he came over to me and set down a plate with some cake on it for me and I'm STILL upset about how well that worked on me! What the heck, Luark. Third hero of the night. He did seem a little better than usual, too. I hope it's real; I'm pretty good at telling with most people, but Luark's had a lot longer than most people to practice hiding it (if he wants to).",
+                    "Then… we all followed the fountain's call back into the Hollow.",
+                ],
+            },
+        ],
+    },
+]
